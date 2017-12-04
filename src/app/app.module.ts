1d29468a437ef292b2694803ab8f7c7620c6f6c7
Sudeep Singn Rawat <sudeepsinghrwt@gmail.com>
2017-12-04 20:04:02 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyNavBarComponent } from './my-nav-bar/my-nav-bar.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { AddMeetingComponent } from './add-meeting/add-meeting.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { MeetingListComponent } from './meeting-list/meeting-list.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNavBarComponent,
+    DashbordComponent,
+    AddMeetingComponent,
+    AddTaskComponent,
+    MeetingListComponent,
+    TaskListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
